198f16d89b2af1d687359ffbfdbb3c3e2071b876
mayank-malhotra007 <makmalhotra07@gmail.com>
2025-07-21 21:00:17 +0200

    added enums

---
+++ MyXcodeCPP/MyXcodeCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyXcodeCPP
+//
+//  Created by optimus on 30.05.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
